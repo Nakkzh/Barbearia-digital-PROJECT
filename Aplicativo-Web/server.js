857c44d1f36982bf420c53296bfb5d6d68c3c26d
JoãoGuilherme <joaoguiguii2310@gmail.com>
2026-04-30 15:33:14 -0300

    Projeto: Configuração

---
+++ Aplicativo-Web/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+class Server
+{
+    app 
+    port
+
+    constructor(port)
+    {
+        this.app = express()
+        this.port = port
+    }
+    
+    listen()
+    {
+        this.app.listen(this.port, () => {
+            console.log("Servidor Online...")
+        })
+    }
+}
+
+module.exports = new Server(3000)
