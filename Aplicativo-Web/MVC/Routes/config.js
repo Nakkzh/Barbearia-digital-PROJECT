07378aa39b6d6c8f0534edd9d50d99a631fff9c1
JoãoGuilherme <joaoguiguii2310@gmail.com>
2026-05-14 09:38:16 -0300

    css upd v.02

--- Aplicativo-Web/MVC/Routes/config.js
+++ Aplicativo-Web/MVC/Routes/config.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const UsuarioController = require('../Controllers/UsuarioController')
 const router = Router()
 
-
 router.get("/user/index", (req, res) => UsuarioController.index(req, res))
 router.post("/user/create", (req, res) => UsuarioController.usuarioPostAsync(req, res))
 router.get("/user/create", (req, res) => UsuarioController.usuarioCreateView(req, res))
